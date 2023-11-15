c187e4a105910359e6375fdaade1fdbf2abf4bdd
Nahuel Escudero <escuderogomezn@gmail.com>
2023-11-14 23:11:01 -0300

    Arreglos esteticos del burger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const buttonBurger = document.querySelector("#burgerButton");
 const navBar = document.querySelector("#navBar");
+const cerrarBurger = document.querySelector("#burger-menu__cerrar");
 
 buttonBurger.addEventListener("click", () => {
-    navBar.classList.toggle("visible");
+    navBar.classList.add("visible");
+});
+
+cerrarBurger.addEventListener("click", () => {
+    navBar.classList.remove("visible");
 });
