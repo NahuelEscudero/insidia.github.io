2fc7d1961c0859520e0d67ed88337edd13cde53b
Nahuel Escudero <escuderogomezn@gmail.com>
2023-11-18 20:16:31 -0300

    Avances en la logica de JS

--- js/main.js
+++ js/main.js
@@ -56,27 +56,50 @@ const remeras = [
 ];
 
 const mainCont = document.querySelector("#mainCont");
+const sumaCarrito = document.querySelector("#sumaCarrito");
+const verCarrito = document.querySelector("#carritoButton");
+
 
 remeras.forEach((item) => {
     let productCard = document.createElement("div");
+
     productCard.classList.add("product-card");
     productCard.innerHTML = `
-    <h4 class="product-card__title">${item.nombre}</h4>
-    <img src="${item.imagen}" alt="Agregar ${item.nombre} al carrito" class="product-card__img">
-    <div class="product-card-add"><p class="product-card-add__precio">${item.precio}</p>
-    <button id="addCarrito${item.id}" class="product-card-add__button"><img src="./images/emojis/ADD CARRITO W.png" alt="Agregar al carrito" class="product-card-add__button-img"></button></div>`;
+        <h4 class="product-card__title">${item.nombre}</h4>
+        <img src="${item.imagen}" alt="Agregar ${item.nombre} al carrito" class="product-card__img">
+        <div class="product-card-add"><p class="product-card-add__precio">${item.precio}</p>
+        <button id="addCarrito${item.id}" class="product-card-add__button"><img src="./images/emojis/ADD CARRITO W.png" alt="Agregar al carrito" class="product-card-add__button-img-white"><img src="./images/emojis/ADD CARRITO B.png" alt="Agregar al carrito" class="product-card-add__button-img-black"></button></div>`;
 
-    mainCont.append(productCard)
+    mainCont.append(productCard);
 
     const addCarritoButton = document.getElementById(`addCarrito${item.id}`);
-    const sumaCarrito = document.querySelector("#sumaCarrito");
+    let valorActual = parseInt(sumaCarrito.innerHTML);
+    const logValorActual = () => {
+        if(verCarrito){
+            verCarrito.addEventListener("click", () => {
+                if(valorActual > 0){
+                    alert("Hay productos en el carrito :)");
+                }else{
+                    alert("No hay productos en el carrito :(");
+                }
+            });
+        }
+        console.log(valorActual);
+    }
 
-    if(addCarritoButton){
+
+    if (addCarritoButton) {
+
         addCarritoButton.addEventListener("click", () => {
-            let valorActual = parseInt(sumaCarrito.innerHTML);
             valorActual += 1;
             sumaCarrito.innerHTML = valorActual;
-        })
+            logValorActual();
+        });
+        logValorActual();
     }
+
+    
+    
+
 });
 
